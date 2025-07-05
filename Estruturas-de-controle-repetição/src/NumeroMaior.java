32042d20aa9d95c6caff9b4285b0b855afee1b5b
fmtic <fmtic@fmtic.com.br>
2025-07-05 17:22:41 -0300

    Exercícios de estrutra de dados

--- Estruturas-de-controle-repetição/src/NumeroMaior.java
+++ Estruturas-de-controle-repetição/src/NumeroMaior.java
@@ -3,16 +3,19 @@ import java.util.Scanner;
 public class NumeroMaior {
 public static void main(String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
-    int num1, num2 = 0;
     System.out.println("Digite o primeiro número: ");
-    num1 = scanner.nextInt();
-    do {
+    var num1 = scanner.nextInt();
+    var verificador = true;
+   while (verificador){
         System.out.println("Digite outro número: ");
-        num2 = scanner.nextInt();
-        if (num2 <= num1) {
-            System.out.println("O segundo número deve ser maior que o primeiro. Tente novamente.");
+        var num2 = scanner.nextInt();
+        if (num2 < num1) {
+            System.out.println("Informe um número maior que " + num1 + ". Tente novamente.");
+            continue;
         }
-    }while (num2 % num1 == 0);
-    System.out.println(num2 + "/" + num1 + " não têm quociente 0");
+        var resultado = num2 % num1;
+        verificador = resultado == 0;
+        System.out.println(num2 +" % "+ num1 + " = " + resultado);
+   		}
     }
 }
